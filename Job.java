709cd100ad9db386da35185f796831427eb12524
JamesGresham <james.gresham@students.mq.edu.au>
2019-04-05 02:49:07 +1100

    Added Job Class, Added logic in schedular to schedule jobs

---
+++ Job.java
@@ -0,0 +1,36 @@
+public class Job {
+
+    //TODO: Edit this class to fit actual structure, this one is wrong.
+
+	public String type;
+    public int submitTime, jobID, estRunTime, numCPUCores, memory, disk;
+
+    public Job(int submitTime, int jobID, int estRunTime, int numCPUCores, int memory, int disk){
+        this.submitTime = submitTime;
+        this.jobID = jobID;
+        this.estRunTime = estRunTime;
+        this.numCPUCores = numCPUCores;
+        this.memory = memory;
+        this.disk = disk;
+    }
+
+    public Job(String inputString){
+        String[] subStrings = inputString.split(" ");
+
+        this.type = subStrings[0];
+        this.submitTime = Integer.parseInt(subStrings[1]);
+        this.jobID = Integer.parseInt(subStrings[2]);
+        this.estRunTime = Integer.parseInt(subStrings[3]);
+        this.numCPUCores = Integer.parseInt(subStrings[4]);
+        this.memory = Integer.parseInt(subStrings[5]);
+        this.disk = Integer.parseInt(subStrings[6]);
+
+    }
+
+    @Override
+    public String toString(){
+        return String.format("node: job attr: type=\"%s\" attr: submitTime=\"%d\" attr: jobID=\"%d\" attr: estRunTime=\"%d\" attr: numCPUCores=\"%d\" attr: memory=\"%d\" attr disk=\"%d\"",
+                this.type, this.submitTime, this.jobID, this.estRunTime, this.numCPUCores, this.memory, this.disk);
+    }
+
+}
--- jobScheduler.java
+++ jobScheduler.java
@@ -1,55 +1,118 @@
 import java.io.*;
 import java.net.*;
- 
+import java.util.ArrayList;
+import java.util.List;
+
 public class jobScheduler
 {
- 
-    private static Socket socket;
- 
-    public static void main(String args[])
-    {
-        try
-        {
-            String host = "localhost";
-            int port = 8096;
-            InetAddress address = InetAddress.getByName(host);
-            socket = new Socket(address, port);
- 
-            //Send the message to the server
-            OutputStream outputStream = socket.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-            BufferedWriter bufferedWriter = new BufferedWriter(
-            		outputStreamWriter);
- 
-            String messageString = "HELO";
- 
-            String sendMessage = messageString;
-            bufferedWriter.write(sendMessage);
-            bufferedWriter.flush();
-            System.out.println("Message sent to the server : "+sendMessage);
- 
-            //Get the return message from the server
-            InputStream inputStream = socket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String message = bufferedReader.readLine();
-            System.out.println("Message received from the server : " +message);
-        }
-        catch (Exception exception)
-        {
-            exception.printStackTrace();
-        }
-        finally
-        {
-            //Closing the socket
-            try
-            {
-                socket.close();
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-    }
+
+	private static Socket socket;
+	private static ObjectOutputStream oos = null;
+	private static ObjectInputStream ois = null;
+	private static ArrayList<Server> servers;
+	private static ArrayList<Job> jobs;
+
+
+	public static void main(String args[])
+	{
+		try
+		{
+			String host = "localhost";
+			int port = 8096;
+			InetAddress address = InetAddress.getByName(host);
+			socket = new Socket(address, port);
+			servers = new ArrayList<>();
+			jobs = new ArrayList<>();
+
+			sendMessage("HELO\n");
+
+			recieveMessage();
+
+			sendMessage("AUTH comp335\n");
+
+			recieveMessage();
+
+			while (true) {
+				sendMessage("REDY\n");
+				String r = recieveMessage();
+				System.out.println(r);
+				
+				if(r.equals("NONE")) {
+					break;
+				}
+				
+				Job j = new Job(r);
+				System.out.println(j.toString());
+				jobs.add(j);
+				
+//				System.out.println(j.jobID);
+				
+				sendMessage("SCHD " + j.jobID + " " + "4xlarge " + "0\n");
+				recieveMessage();
+
+			}
+			
+			
+			sendMessage("RESC All\n");
+
+			recieveMessage();
+
+//			sendMessage("OK\n");
+//			recieveMessage();
+			
+
+			while (true) {
+				sendMessage("OK\n");
+				String r = recieveMessage();
+
+				if(r.equals(".")){                     
+					break;
+				}
+				
+				Server s = new Server(r);
+				System.out.println(s.toString());
+				servers.add(s);
+			}
+			
+			sendMessage("OK");
+			recieveMessage();
+
+		}
+		catch (Exception exception)
+		{
+			exception.printStackTrace();
+		}
+		finally
+		{
+			//Closing the socket
+			try
+			{
+				socket.close();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+	static void sendMessage(String message) throws IOException {
+		OutputStream outputStream = socket.getOutputStream();
+		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+		BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+		bufferedWriter.write(message);
+		bufferedWriter.flush();
+		System.out.println("Message sent to the server : "+message);
+	}
+
+	static String recieveMessage() throws IOException {
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		String message = br.readLine();
+		System.out.println("Message received from the server : " +message);
+
+		return message;
+	}
 }
